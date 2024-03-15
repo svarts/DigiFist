ed7cb519f43af66fc2a5cb7652fc33c716d42d98
Esma Karaca <killbillbeatrixx@gmail.com>
2024-03-16 01:15:45 +0300

    Implement default color selection and visual feedback.

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     const colorsData = {
-        'colors-1': ['#99C3CC', '#CC9999', '#CB99CC', '#A6CC99'],
-        'colors-2': ['#99C3CC', '#CC9999', '#CB99CC', '#A6CC99'],
-        'colors-3': ['#99C3CC', '#CC9999', '#CB99CC', '#A6CC99']
+        "colors-1": ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
+        "colors-2": ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
+        "colors-3": ["#99C3CC", "#CC9999", "#CB99CC", "#A6CC99"],
     };
 
     for (let key in colorsData) {
         const colorContainer = document.getElementById(key);
         if (colorContainer) {
-            colorsData[key].forEach(color => {
-                const colorSpan = document.createElement('span');
-                colorSpan.classList.add('color');
+            let firstColor = true;
+            colorsData[key].forEach((color) => {
+                const colorSpan = document.createElement("span");
+                colorSpan.classList.add("color");
                 colorSpan.style.backgroundColor = color;
-                colorSpan.addEventListener('click', function () {
-                    document.querySelectorAll(`#${key} .color`).forEach(c => c.classList.remove('selected'));
+                colorSpan.addEventListener("click", function () {
+                    document
+                        .querySelectorAll(`#${key} .color`)
+                        .forEach((c) => c.classList.remove("selected"));
 
-                    this.classList.add('selected');
+                    this.classList.add("selected");
                 });
                 colorContainer.appendChild(colorSpan);
+
+                if (firstColor) {
+                    colorSpan.classList.add("selected");
+                    firstColor = false;
+                }
             });
         }
     }
 });
-
